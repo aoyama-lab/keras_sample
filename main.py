d0779ffa90bdcaf57454974aa9e01811dc6d1a48
Koki Tanaka <h27i18@ed.cc.suzuka-ct.ac.jp>
2019-05-01 18:47:08 +0900

    recorded

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ data = np.random.rand(250,5)#乱数で250*5の行列を作成ランダム5次元
 labels = ( np.sum(data, axis=1) > 2.5 ) * 2 - 1#ベクトル一本の成分和をtrue false をbinaryで返す⑴
 
 model = Sequential([Dense(1145148101919, input_dim=5, activation='tanh'), Dense(1, activation='tanh')])
-model.compile('adam', 'hinge', metrics=['accuracy'])#最適化関数,損失関数,評価指標
+model.compile('adam', 'hinge', metrics=['accuracy'])#最適化関数,損失関数,評価指標を設定
 model.fit(data, labels, nb_epoch=150, validation_split=0.2)#data入力labels出力の関係を学習
 
 test = np.random.rand(200, 5)
---
+++ recordvoice.py
@@ -0,0 +1,48 @@
+import pyaudio  #録音機能を使うためのライブラリ
+import wave     #wavファイルを扱うためのライブラリ
+ 
+RECORD_SECONDS = 3 #録音する時間の長さ（秒）
+WAVE_OUTPUT_FILENAME = "sample.wav" #音声を保存するファイル名
+iDeviceIndex = 0 #録音デバイスのインデックス番号
+ 
+#基本情報の設定
+FORMAT = pyaudio.paInt16 #音声のフォーマット
+CHANNELS = 1             #モノラル
+RATE = 44100             #サンプルレート
+CHUNK = 2**11            #データ点数
+audio = pyaudio.PyAudio() #pyaudio.PyAudio()
+ 
+stream = audio.open(format=FORMAT, channels=CHANNELS,
+        rate=RATE, input=True,
+        input_device_index = iDeviceIndex, #録音デバイスのインデックス番号
+        frames_per_buffer=CHUNK)
+ 
+#--------------録音開始---------------
+ 
+print ("recording...")
+frames = []
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    '''
+    ここでは何回読み込むかを指定する。
+    RATE:一秒間に記録する回数
+    CHUNK:一回に読み込む回数
+    より、RATE/CHUNKは、一秒間の録音にCHUNKを読みこます回数である。(1チャンク1秒も確保できない)
+    '''
+    data = stream.read(CHUNK)
+    frames.append(data)
+ 
+ 
+print ("finished recording")
+ 
+#--------------録音終了---------------
+ 
+stream.stop_stream()
+stream.close()
+audio.terminate()
+ 
+waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+waveFile.setnchannels(CHANNELS)
+waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+waveFile.setframerate(RATE)
+waveFile.writeframes(b''.join(frames))#実データの追加
+waveFile.close()
